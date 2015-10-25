be67818ffea2716208a3fad013918cd3f3e68c74
Anton Kadantsev <kadantsev.anton@yandex.ru>
2015-10-25 12:45:24 +0300

    Закончил урок 1. Работал с welcom

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -104,6 +104,7 @@ if (isset($_SERVER['KOHANA_ENV']))
  */
 Kohana::init(array(
 	'base_url'   => '/kohana/',
+        'index_file' => false,
 ));
 
 /**
@@ -144,8 +145,10 @@ Kohana::modules(array(
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
-Route::set('default', '(<controller>(/<action>(/<id>)))')
+Route::set('default', '(<controller>(/<action>(/<id>(/<lostName>(/<age>)))))')
 	->defaults(array(
 		'controller' => 'welcome',
 		'action'     => 'index',
 	));
+
+
