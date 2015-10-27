2d8e62219b715a7c01aab5599d87818fa0929dd1
Anton Kadantsev <kadantsev.anton@yandex.ru>
2015-10-27 22:44:35 +0300

    добавил display flex

--- application/views/v_base.php
+++ application/views/v_base.php
@@ -8,53 +8,71 @@
 </head>
 <body>
 <style  type="text/css">
+//пример здесь http://www.sitehere.ru/kolonki-odinakovoj-vysoty-na-css
 body {
-  color: #000; 
-  background: #FFFFFF;
-  word-wrap: break-word;
-  font-size: 12px; 
-  font-family: Verdana, Arial, Sans-Serif; 
+    color: #000; 
+    background: #FFFFFF;
+    word-wrap: break-word;
+    font-size: 12px; 
+    font-family: Verdana, Arial, Sans-Serif; 
 }
 #header {
-  height:100px;
-  background: #f86a6a ;
+    height:100px;
+    background: #f86a6a;
+
 }
 #header h1{
     text-align: center;
     padding-top: 30px;
 }
 #container {
-  min-width:800px;
-} 
+    display: flex;
+    min-width:1200px;
+    margin: 0px auto;
+}
 #center {
-  margin:0px 200px 0px 200px;
-  background: #ecf2db ;
+   // margin:0px 200px 0px 200px;
+    background: #ecf2db;
+    //display: table-cell;
+    min-width: 800px;
+    //flex-grow:1;
+
 }
 #left {
-  float:left; 
-  width:200px;
-  background: #fcbfaf;
+    float:left; 
+    width:200px;
+    background: #fcbfaf;
+    flex-shrink:0;
+    //display: table-cell;
 }
 #right {
-  float:right; 
-  width:200px;
-  background: #fcbfaf;
+    float:right; 
+    width:200px;
+    background: #fcbfaf;
+    flex-shrink:0;
+    //display: table-cell;
 }
 #right h3{
     text-align: center;
 }
 #footer {
-  height:100px;
-  background: #6c9f8b;
+    height:100px;
+    background: #6c9f8b;
+    text-align: center;
+
 }
 .clear {
-  clear:both;
+    clear:both;
 }
-
+#conUp
+{
+    display: flex; 
+}
 </style>
 <div id="wrapper">
     <div id="header"><h1><?=$title?></h1></div>
-	<div id="container">
+    <div id="conUp">
+        <div id="container">
             <div id="left">
                 <ul>
                 <?php foreach($menu as $name => $href): ?>
@@ -62,13 +80,15 @@ body {
                 <?php endforeach?>
                 </ul>
             </div>
-		<div id="right"><h3>Лучшей товар</h3><?=$best?></div>
-		<div id="center">
-                    <?=$content?>
-                </div>
-		<div></div>
-	</div>
+            <div id="center">
+                <?=$content?>
+            </div>
+            <div id="right"><h3>Лучшей товар</h3><?=$best?></div>
+
+            <div closs="clear"></div>
+        </div>
+    </div>
 </div>
-<div id="footer"><?=$footer?></div>
+    <div id="footer"><?=$footer?></div>
 </body>
 </html>
