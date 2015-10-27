04ed3028092ca5168aa17a3363229c7af204b4e0
Anton Kadantsev <kadantsev.anton@yandex.ru>
2015-10-27 16:12:04 +0300

    Шаблон 1вариант

--- application/classes/Controller/index.php
+++ application/classes/Controller/index.php
@@ -1,23 +1,13 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Index extends Controller_Template {
-    
-        public $template = 'v_index';
-
-	public function action_index()
-	{
-            $this->template->title = 'Интернет-магазин';
-            $this->template->content = 'Главная страница';
-	}
-	public function action_catalog()
-	{
-            $products = array(
-                'Товар 1' => 100,
-                'Товар 2' => 200,
-            );
-            $this->template->title = 'Интернет-магазин';
-            $this->template->content = View::factory('v_catalog', array('products' => $products));
-	}
-
-
-} 
+class Controller_Index extends Controller_Template{
+    public $template = 'v_base';
+    public function action_index()
+    {
+        $this->template->title = 'Интернет магазин';
+    }
+    public function action_catalog()
+    {
+        
+    }
+}
---
+++ application/classes/Model/catalog.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Model_Catalog extends Model {
+
+	public function all_products()
+	{
+            return array('Товар 1' => 100,
+                'Товар 2' => 200,
+                'Товар 3' => 300,
+                'Товар 4' => 400,
+                );
+        }
+}
