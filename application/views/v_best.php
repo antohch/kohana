7acbb6e66d0606f94a6d6e9cb969f23d8b1369ff
Anton Kadantsev <kadantsev.anton@yandex.ru>
2015-10-28 10:42:35 +0300

    Добавил шаблон на лучшего товара

---
+++ application/views/v_best.php
@@ -0,0 +1,10 @@
+<table border="0" width="100%">
+    <tbody>
+        <?php foreach($best as $product => $cost):?>
+        <tr>
+            <td><?=$product?></td>
+        </tr>
+        <?php endforeach ?>
+    </tbody>
+</table>
+
