98fb6e1b4c538f32d3cc490778388ec17d15df12
Anton Kadantsev <kadantsev.anton@yandex.ru>
2015-11-07 20:02:55 +0300

    Добавил новости

--- application/classes/Controller/index/bay.php
+++ application/classes/Controller/index/bay.php
@@ -6,13 +6,10 @@ class Controller_Index_Bay extends Controller_Index{
         //получить виджет
         $menu = Request::factory('widgets/menu')->execute();
         $top = Request::factory('widgets/topproducts')->execute();
-        
-
+       
         //вывести в шаблон
         $this->template->block_left = array($menu);
         $this->template->block_right = array($top);
-       
-        
     }
     public function action_index(){
         //получить данные из модуля и обработать
--- application/classes/Controller/widgets.php
+++ application/classes/Controller/widgets.php
@@ -5,6 +5,7 @@ class Controller_Widgets extends Controller_Template{
     //чтобы на прямую не попасть в виджет
     public function before(){
         parent::before();
+        
         if(Request::current()->is_initial()){
             $this->auto_render = FALSE;
         }
