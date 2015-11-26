267fd42b30fd01b1acfbf84e0119ccc0a7bc95e7
Anton Kadantsev <kadantsev.anton@yandex.ru>
2015-11-26 17:23:07 +0300

    сессия

--- application/views/index/v_bay.php
+++ application/views/index/v_bay.php
@@ -1,10 +1,15 @@
-<table>
+<table border="1">
     <tr>
         <td>Название</td>
         <td>Количество</td>
         <td>Цена</td>
     </tr>    
-<?php foreach($products as $cont):?>
+<?php foreach($products as $product):?>
+    <tr>
+        <td><?=$product->title?></td>
+        <td><?php echo $products_s[$product->id]?></td>
+        <td><?php echo $product->cost * $products_s[$product->id]?></td>
+    </tr>
 <?php endforeach?>
 
 </table>
